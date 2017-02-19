b68e21e3fac52d59f1699abc25d77d02520ffbf3
Waterstrong <sqlin@thoughtworks.com>
2017-02-19 21:52:45 +0800

    add case for less than two alive neightbours

--- src/js/conway.js
+++ src/js/conway.js
@@ -1,9 +1,33 @@
 const gotoNextGeneration = (grid) => {
-    return grid.map((row) => {
-        return row.map((cell) => {
+    return grid.map((row, rowIndex) => {
+        return row.map((cell, colIndex) => {
+            let aliveNeighborNumber = calculateAliveNeighborNumber(rowIndex, colIndex, grid);
+            if(aliveNeighborNumber < 2) {
+                return 0;
+            }
             return cell;
         });
     });
+};
+
+const calculateAliveNeighborNumber = (rowIndex, colIndex, grid) => {
+    let aliveNeighborNumber = 0;
+    for (let i = -1; i < 2; ++i) {
+        for (let j = -1; j < 2; ++j) {
+            if (i !== 0 && j !== 0) {
+                let newRowIndex = rowIndex + i;
+                let newColIndex = colIndex + j;
+                if (newRowIndex >= 0 && newRowIndex < grid.length
+                    && newColIndex >= 0 && newColIndex < grid[newRowIndex].length) {
+                    if (grid[newRowIndex][newColIndex] === 1) {
+                        ++aliveNeighborNumber;
+                    }
+                }
+
+            }
+        }
+    }
+    return aliveNeighborNumber;
 };
 
 module.exports = {
--- test/conway.spec.js
+++ test/conway.spec.js
@@ -10,4 +10,16 @@ describe('Test Conway Game of Life', () => {
         expect(next).to.be.instanceof(Array);
         expect(next.toString()).to.equal(current.toString());
     });
+
+    it('should next state be the all dead given the grid with less than two alive cells', () => {
+        let current1 = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+        let current2 = [[0, 1, 0], [0, 1, 0], [0, 0, 0]];
+        let expectNext = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+        [current1, current2].forEach((current) => {
+            let next = gotoNextGeneration(current);
+            expect(next).be.instanceof(Array);
+            expect(next.toString()).to.equal(expectNext.toString());
+        });
+    });
 });
