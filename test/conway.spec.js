77bdbfabd3986b20ef8f1fb7e826ddfa49ce7e43
Waterstrong <sqlin@thoughtworks.com>
2017-02-19 22:48:35 +0800

    format the test case to make them more readable

--- test/conway.spec.js
+++ test/conway.spec.js
@@ -3,15 +3,31 @@ import {gotoNextGeneration} from '../src/js/conway.js';
 
 describe('Test Conway Game of Life', () => {
     it('should next state be the same given the grid with zero alive cell', () => {
-        let current = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        let current = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
 
         assertNextGeneration(gotoNextGeneration(current), current);
     });
 
     it('should next state be the all dead given the grid with less than two alive cells', () => {
-        let current1 = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
-        let current2 = [[0, 1, 0], [0, 1, 0], [0, 0, 0]];
-        let expectNext = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        let current1 = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ];
+        let current2 = [
+            [0, 1, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ];
+        let expectNext = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
 
         [current1, current2].forEach((current) => {
             assertNextGeneration(gotoNextGeneration(current), expectNext);
@@ -19,29 +35,61 @@ describe('Test Conway Game of Life', () => {
     });
 
     it('should next state be correct given the grid with one alive or dead cell has two alive neighbours', () => {
-        let current = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-        let expectNext = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+        let current = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ];
+        let expectNext = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ];
 
         assertNextGeneration(gotoNextGeneration(current), expectNext);
     });
 
     it('should next state be correct given the grid with one alive or dead cell has three alive neighbours', () => {
-        let current = [[0, 1, 0], [0, 1, 1], [0, 1, 0]];
-        let expectNext = [[0, 1, 1], [1, 1, 1], [0, 1, 1]];
+        let current = [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 1, 0]
+        ];
+        let expectNext = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [0, 1, 1]
+        ];
 
         assertNextGeneration(gotoNextGeneration(current), expectNext);
     });
 
     it('should next state be correct given the grid with one alive cell has more than three alive neighbours', () => {
-        let current = [[0, 1, 0], [1, 1, 1], [0, 1, 0]];
-        let expectNext = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+        let current = [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0]
+        ];
+        let expectNext = [
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ];
 
         assertNextGeneration(gotoNextGeneration(current), expectNext);
     });
 
     it('should next state be correct given the grid with one dead cell has more than three alive neighbours', () => {
-        let current = [[0, 1, 0], [1, 0, 1], [0, 1, 0]];
-        let expectNext = [[0, 1, 0], [1, 0, 1], [0, 1, 0]];
+        let current = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ];
+        let expectNext = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ];
 
         assertNextGeneration(gotoNextGeneration(current), expectNext);
     });
