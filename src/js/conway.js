2ca7c9a0865567068344e218d49fbfbd082a3096
Waterstrong <sqlin@thoughtworks.com>
2017-06-03 14:06:03 +0800

    refactor the code again and update package file

--- src/js/conway.js
+++ src/js/conway.js
@@ -9,27 +9,29 @@ const NEIGHBOR_INDEX = [
     [1, 1]
 ];
 
-const gotoNextGeneration = (grid) => {
-    return grid.map((row, rowIndex) => {
-        return row.map((cell, colIndex) => {
-            let aliveNeighborNumber = calculateAliveNeighborNumber(rowIndex, colIndex, grid);
+const gotoNextGeneration = (grid) =>
+    grid.map((row, rowIndex) =>
+        row.map((cell, colIndex) => {
+            const aliveNeighborNumber = calculateAliveNeighborNumber(rowIndex, colIndex, grid);
             return getNextState(grid[rowIndex][colIndex], aliveNeighborNumber);
-        });
-    });
-};
+        }));
 
 const getNextState = (currentState, aliveNeighborNumber) =>
     [0, 0, currentState, 1, 0, 0, 0, 0][aliveNeighborNumber];
 
 const calculateAliveNeighborNumber = (rowIndex, colIndex, grid) =>
     NEIGHBOR_INDEX
-        .map(position => transformIndexToState(rowIndex + position[0], colIndex + position[1], grid))
+        .map(position => transformIndexToState(position, rowIndex, colIndex, grid))
         .reduce((result, state) => result + state, 0);
 
-const transformIndexToState = (rowIndex, colIndex, grid) =>
-    withinIndex(rowIndex, colIndex, grid) ? grid[rowIndex][colIndex] : 0;
+const transformIndexToState = (position, rowIndex, colIndex, grid) => {
+    const newRowIndex = rowIndex + position[0];
+    const newColIndex = colIndex + position[1];
+    return withinIndex(newRowIndex, newColIndex, grid) ? grid[newRowIndex][newColIndex] : 0;
+};
+
 
 const withinIndex = (rowIndex, colIndex, grid) =>
-    rowIndex >= 0 && rowIndex < grid.length && colIndex >= 0 && colIndex < grid[rowIndex].length;
+rowIndex >= 0 && rowIndex < grid.length && colIndex >= 0 && colIndex < grid[rowIndex].length;
 
 export default gotoNextGeneration;
